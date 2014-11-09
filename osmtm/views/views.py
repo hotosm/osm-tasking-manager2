56d919fa14efa8350fea622786e1b2ca38371001
Ethan <ethan-nelson@users.noreply.github.com>
2014-11-08 19:05:16 -0600

    adding tour page to views

--- osmtm/views/views.py
+++ osmtm/views/views.py
@@ -122,7 +122,10 @@ def user_prefered_language(request):
     request.response.set_cookie('_LOCALE_', value=language,
                                 max_age=20 * 7 * 24 * 60 * 60)
     return dict()
-
+    
+@view_config(route_name='tour', renderer='tour.mako')
+def tour(request):
+    return dict()
 
 @view_config(context='pyramid.httpexceptions.HTTPUnauthorized')
 def unauthorized(request):
