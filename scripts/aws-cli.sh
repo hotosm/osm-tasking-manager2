160951a341124a67fa1758055bd4dc5222e70c17
David Neudorfer <david@davidneudorfer.com>
2017-01-18 21:08:25 -0800

    install aws cli through pip

--- scripts/aws-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-sudo apt-get install unzip
-unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-aws --version
