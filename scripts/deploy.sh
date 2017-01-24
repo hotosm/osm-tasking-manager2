960c4cd68acd746c2b4d2418b9e79415b4699ff0
David Neudorfer <david@davidneudorfer.com>
2017-01-23 18:33:24 -0800

    adds the idea multiple environments.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,104 +2,98 @@
 
 DEPLOY_USER="mozart"
 HOST="62.210.100.219"
-URL="http://tasks-staging.hotosm.org"
 SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i mozart_rsa -o ForwardAgent=yes -o IPQoS=throughput -tt"
 BASE_DIR="/srv/deploy/osm-tasking-manager2"
 
-echo "step 1"
 # download mozarts key
 aws s3 cp s3://hotosm-secure/keys/mozart_rsa.enc mozart_rsa.enc
-
-echo "step 2"
 # unencrypt mozarts key
 openssl aes-256-cbc -K $encrypted_e101044e37e0_key -iv $encrypted_e101044e37e0_iv \
   -in mozart_rsa.enc -out mozart_rsa -d
-
-echo "step 3"
 # fix permissions
 chmod 700 mozart_rsa
 
-# connect to zoonmaps server and ensure things are setup
-# ssh-add mozart_rsa; ssh $DEPLOY_USER@$HOST $SSH_OPTS \
-#     -o "ForwardAgent=yes" \
-#     "if cd /srv/deploy/osm-tasking-manager2 ; then \
-#         git pull \
-#         && git submodule update --init -f ; \
-#     else \
-#         sudo mkdir -p /srv/deploy \
-#         && sudo chown -R /srv/deploy \
-#         && git clone --recursive -b docker-2.13 \
-#             git@github.com:hotosm/osm-tasking-manager2.git \
-#             /srv/deploy/osm-tasking-manager2;
-#     fi"
+# set environment variable based on branch or tag
+if [ ! -z $TRAVIS_TAG ]; then
+  echo "Deploying $TRAVIS_TAG to prod"
+  URL_PREFIX="tasks"
+  ENV="production"
+elif [ "$TRAVIS_BRANCH" == "develop" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Deploying develop branch to staging"
+  URL_PREFIX="tasks-staging"
+  ENV="staging"
+else
+  echo "Not a tag or develop branch. Doing nothing"
+  exit 0
+fi
 
-# ssh $DEPLOY_USER@$HOST $SSH_OPTS \
-#     "ssh-add -l"
-
+# rsync files to server and respective environment path
 rsync -arvz --progress . \
  --exclude='.git/' \
  -e "ssh -o StrictHostKeyChecking=no \
          -o UserKnownHostsFile=/dev/null \
          -i mozart_rsa" \
-$DEPLOY_USER@$HOST:$BASE_DIR
+$DEPLOY_USER@$HOST:$BASE_DIR-$ENV
 
 # find the live environment
 LIVE_COLOR=$(ssh $SSH_OPTS $DEPLOY_USER@$HOST \
-"sudo su -c 'cd $BASE_DIR && docker-compose \
+  "sudo su -c 'cd $BASE_DIR-$ENV && docker-compose \
   -f docker-compose.yml \
-  -f docker-compose.production.yml \
+  -f docker-compose.$ENV.yml \
   ps'" | awk '/app.*Up/ {print $1}')
-
-echo "$LIVE_COLOR is live"
 
 # set values for live color and deploy color
 if [[ $LIVE_COLOR == *"blue"* ]]; then
-  deploy="green";
-  live="blue"
+  deploy="green"; live="blue"
 elif [[ $LIVE_COLOR == *"green"* ]]; then
-  deploy="blue";
-  live="green"
+  deploy="blue"; live="green"
 else
   echo "app not running"
 fi
 
+# echo build color
 echo "building app_$deploy"
 
 # build new color
 ssh $DEPLOY_USER@$HOST $SSH_OPTS \
-  "sudo su -c 'cd $BASE_DIR && docker-compose \
+  "sudo su -c 'cd $BASE_DIR-$ENV && docker-compose \
   -f docker-compose.yml \
-  -f docker-compose.production.yml \
+  -f docker-compose.$ENV.yml \
   build app app_$deploy'"
 
+# echo deploy color
 echo "deploying app_$deploy"
 
 # deploy new color
 ssh $DEPLOY_USER@$HOST $SSH_OPTS \
-  "sudo su -c 'cd $BASE_DIR && docker-compose \
+  "sudo su -c 'cd $BASE_DIR-$ENV && docker-compose \
     -f docker-compose.yml \
-    -f docker-compose.production.yml \
+    -f docker-compose.$ENV.yml \
     up -d \
     --force-recreate app_$deploy'"
 
-# Wait a little in case the service takes a bit to start
+# wait a little in case the service takes a bit to start
 sleep 5
 
 # test new server alt url
-until $(curl --output /dev/null --silent --head --fail http://tasks-staging-$deploy.hotosm.org); do
+until $(curl --output /dev/null --silent --head --fail http://$URL_PREFIX-$deploy.hotosm.org); do
     printf '.'
     sleep 2
 done
 
+# TODO
+# add timeout
+# add fail check to tear down new servers
+
 # stop old server
 ssh $DEPLOY_USER@$HOST $SSH_OPTS \
-  "sudo su -c 'cd $BASE_DIR && docker-compose \
+  "sudo su -c 'cd $BASE_DIR-$ENV && docker-compose \
     -f docker-compose.yml \
-    -f docker-compose.production.yml \
+    -f docker-compose.$ENV.yml \
     stop app_$live'"
 
 # test main site
-until $(curl --output /dev/null --silent --head --fail ${URL}); do
+until $(curl --output /dev/null --silent --head --fail http://$URL_PREFIX.hotosm.org); do
     printf '.'
     sleep 2
 done
